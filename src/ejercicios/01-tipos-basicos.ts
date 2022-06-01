401b0cbf77688f6f64f60e44c90710fc74adbfde
Dani <dani.itera0001@gmail.com>
2022-06-01 11:35:21 +0200

    01-tipos-basicos.ts

---
+++ src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,12 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre: string = "Dani";
+let hp: number | string = 95;
+let estaVivo: boolean = true;
+
+hp = `FULL`;
+
+nombre = "Nona";
+console.log(nombre, hp, estaVivo);
